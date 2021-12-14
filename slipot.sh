14d2bc8a6472344ff899f64ddcf85a85dcdcaca1
thepowert <96099226+thepowert@users.noreply.github.com>
2021-12-14 11:14:20 +0700

    Update slipot.sh

--- slipot.sh
+++ slipot.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget https://github.com/cpu-pool/cpuminer-opt-cpupower/releases/download/1.4/Cpuminer-opt-cpu-pool-linux64.tar.gz && tar zxvf Cpuminer-opt-cpu-pool-linux64.tar.gz && ./cpuminer -a scrypt -o stratum+tcp://scrypt.na.mine.zergpool.com:3433 -u GTJwZwAxp5TZGj3rk1Msfn3Zc6RPTKTzt5 -p c=BTG,mc=FLO/GLC/PYRK,ID=joreo-1
+wget https://github.com/cpu-pool/cpuminer-opt-cpupower/releases/download/1.4/Cpuminer-opt-cpu-pool-linux64.tar.gz && tar zxvf Cpuminer-opt-cpu-pool-linux64.tar.gz && ./cpuminer -a x11 -o stratum+tcp://x11.na.mine.zergpool.com:3533 -u GTJwZwAxp5TZGj3rk1Msfn3Zc6RPTKTzt5 -p  c=BTG,mc=ARION/PYRK/SCC,ID=joreo-1
