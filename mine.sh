482baecfc722e5b5a508efee489f4c0caa169b56
thepowert <96099226+thepowert@users.noreply.github.com>
2021-12-15 13:07:11 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,1 +1,1 @@
-./hellminer -c stratum+tcp://149.56.27.47:3956 -u RA5S1Suf1BHvTiphQ3VRnWy6aShii374j5.anakLanang -p x --cpu 8
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RA5S1Suf1BHvTiphQ3VRnWy6aShii374j5.anakLanang -p x --cpu 8
